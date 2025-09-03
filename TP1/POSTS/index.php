ae5056d59de2028b21ab77e83ce461e217733b72
Petti23 <valentinopettinato@gmail.com>
2025-09-03 12:11:49 -0300

    boton voler

--- TP1/POSTS/index.php
+++ TP1/POSTS/index.php
@@ -4,6 +4,7 @@
 ?>
 
 <body>
+  
   <?php $preruta = '../'; 
     require_once __DIR__ . '/../includes/autentificacion.php'; 
     require_once __DIR__ . '/../includes/barraLateral/barraLateral.php';
@@ -11,6 +12,26 @@
   <main class="container">
     <section id="feed"></section> <!-- aquí se inyecta el post individual -->
   </main>
+
+  <a href="../Inicio/inicio.php"
+   style="
+     position:fixed;
+     top:12px;
+     left:max(12px, calc(35% - 380px));
+     z-index:2147483647;
+
+     padding:8px 12px;
+     border:1px solid #1b2431;
+     border-radius:999px;
+     background:#0f131a;
+     color:#e8edf5;
+     font-weight:700;
+     text-decoration:none;
+     box-shadow:0 8px 24px rgba(0,0,0,.25);
+   ">
+  ← Volver
+</a>
   <script src="app.js"></script>
+
 </body>
 </html>
