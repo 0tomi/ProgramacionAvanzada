889459bfd0ae1dda57798a4464df132934b63cb5
tomas schlotahuer <tomsschlotahuer@hotmail.com>
2025-09-03 22:46:46 -0300

    .

--- TP1/includes/procesoLogin.php
+++ TP1/includes/procesoLogin.php
@@ -9,9 +9,11 @@ $usuarios = leerUsuarios();
 foreach ($usuarios as $user) {
     if ($user['username'] === $username && password_verify($password, $user['password'])) {
         session_start();
+        // Proximamente esto ira dentro de una clase, no llegamos
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $username;
         $_SESSION['user_profile_picture'] = $user['user_profile_picture'];
+        $_SESSION['description'] = $user['description'];
         $_SESSION['flash'] = ['type' => 'success', 'msg' => 'Inicio de sesión correcto'];
         header("Location: ../Inicio/inicio.php");
         exit;
