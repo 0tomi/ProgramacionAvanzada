0a79ab6a61598b413cae3061fe5a4afaaa42d4b5
tomygiordev <tomasroldangiorgi@gmail.com>
2025-09-27 18:38:22 -0300

    chau config

--- TP1/LOGIN/_login.php
+++ TP1/LOGIN/_login.php
@@ -1,5 +1,4 @@
 <?php
-require_once __DIR__ . '/../config.php';
 ?>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
