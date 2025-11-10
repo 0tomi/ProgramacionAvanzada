b0570797469cc3f29851654fe4a962029ad20654
tomas schlotahuer <tomsschlotahuer@hotmail.com>
2025-11-10 16:10:59 -0300

    d

--- Views/header.php
+++ Views/header.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <script src="https://cdn.tailwindcss.com"></script>
+
 
   <?php if (!empty($require_boostrap)): ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
